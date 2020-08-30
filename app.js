aae6503adcd90218b13df09629ba7dd6c284762b
leumas <LeumasCode@users.noreply.github.com>
2020-08-30 15:28:13 -0100

    first commit on the Natours api

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('hello from middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// route handler
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/Users', userRouter);
+
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+      status: 'err',
+      message: 'Route not yet ready',
+    });
+  };
+  
+  exports.getUser = (req, res) => {
+    res.status(500).json({
+      status: 'err',
+      message: 'Route not yet ready',
+    });
+  };
+  
+exports.createUser = (req, res) => {
+    res.status(500).json({
+      status: 'err',
+      message: 'Route not yet ready',
+    });
+  };
+  
+  exports.updateUser = (req, res) => {
+    res.status(500).json({
+      status: 'err',
+      message: 'Route not yet ready',
+    });
+  };
+  
+  exports.deleteUser = (req, res) => {
+    res.status(500).json({
+      status: 'err',
+      message: 'Route not yet ready',
+    });
+  };
+  
