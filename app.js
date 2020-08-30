a6503b078bd3ca5a5ee47fadc80f3c16fe768b29
leumas <LeumasCode@users.noreply.github.com>
2020-08-30 15:31:13 -0100

    remove the time middleware

--- app.js
+++ app.js
@@ -18,11 +18,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
 // route handler
 
 app.use('/api/v1/tours', tourRouter);
