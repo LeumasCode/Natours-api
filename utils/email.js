8add2c0245264a636f03d77003bffd831f964814
unknown <opeyemisamuel222@gmail.com>
2020-11-13 21:07:29 +0100

    email template created and email class working with the welcome method

--- utils/email.js
+++ utils/email.js
@@ -1,36 +1,59 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const hmtlToText = require('html-to-text');
 
 // new Email(user, url).sendEmail()
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
-    this.firstName = user.split(' ')[0];
+    this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Samuel <${process.env.EMAIL_FROM}>`
+    this.from = `Samuel <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // SendGrid
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    // 1) RENDER HTML BASE ON A PUG TEMPLATE
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: hmtlToText.fromString(html),
+      //html:
+    };
+
+    // 3) create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'welcome to the natours family!');
   }
 };
-
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // 2) define email options
-  const mailOptions = {
-    from: 'Samuel <hello@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    //html:
-  };
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
-};
-
-module.exports = sendEmail;
